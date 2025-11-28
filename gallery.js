9adccbad3bc1ad10098397fce8211667ff15a03a
Talida-ux <talidabusiness@gmail.com>
2025-11-28 15:07:57 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -10,4 +10,14 @@ function unDo(){
     var imgElem = document.getElementById("image");
     imgElem.style.backgroundImage = "url('')";
     imgElem.innerHTML = "Hover over an image below to display here.";
+
 }
+function addTabIndex() {
+    const images = document.querySelectorAll('img');
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute('tabindex', 0); // 0 allows keyboard tabbing
+        console.log(`Tabindex added to image ${i+1}`);
+    }
+}
+
+window.onload = addTabIndex;
